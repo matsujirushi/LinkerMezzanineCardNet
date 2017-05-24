879dd0d0f2e8eace2e40454a7ab1a4183d474a94
matsujirushi <matsujirushi@live.jp>
2017-05-25 00:30:01 +0900

    Add PinName and ConnectorPinName.

---
+++ src/LinkerMezzanineCardNet/ConnectorPinName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkerMezzanineCardNet
+{
+    public enum ConnectorPinName
+    {
+        D_A = DragonBoard410c.PinName.J8_23,
+        D_B = DragonBoard410c.PinName.J8_24,
+        D_C = DragonBoard410c.PinName.J8_25,
+        D_D = DragonBoard410c.PinName.J8_26,
+        D_E = DragonBoard410c.PinName.J8_27,
+        D_J = DragonBoard410c.PinName.J8_32,
+        D_G = DragonBoard410c.PinName.J8_29,
+        D_H = DragonBoard410c.PinName.J8_30,
+    }
+}
---
+++ src/LinkerMezzanineCardNet/DragonBoard/PinName.cs
@@ -0,0 +1,19 @@
+﻿namespace DragonBoard410c
+{
+    public enum PinName
+    {
+        J8_23 = 36,
+        J8_24 = 12,
+        J8_25 = 13,
+        J8_26 = 69,
+        J8_27 = 115,
+        J8_29 = 24,
+        J8_30 = 25,
+        J8_31 = 35,
+        J8_32 = 34,
+        J8_33 = 28,
+        J8_34 = 33,
+        USER_LED1 = 21,
+        USER_LED2 = 120,
+    }
+}
